865140088b6419295d325d125dbe8fbc7e268fbb
Sujanravichandran <161161877+Sujanravichandran@users.noreply.github.com>
2024-10-04 11:39:20 +0530

    Add files via upload

---
+++ Print the sum of the cubes of the values from m to n.js
@@ -0,0 +1,18 @@
+let m= prompt("Enter the value of m")
+let n= prompt("Enter the value of n")
+let sum=0
+if(m>n){
+    console.log("0");
+}
+else{
+    for(let i=m;i<=n;i++){
+        sum+=(i*i*i)
+    }
+    console.log(sum);
+}
+
+
+
+
+
+
